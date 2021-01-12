97bdb4aa5ea7a021896872efb23dbb0fc25b2506
SparklingFun <sparkinglemon@foxmail.com>
2021-01-12 09:34:34 +0000

    update: add entry

---
+++ index.js
@@ -0,0 +1,46 @@
+// `npx create-sparkling-app <template> <dirname>`
+const fs = require('fs-extra');
+const argv = process.argv;
+
+function execCopy(type, tplname, topath) {
+    try {
+        fs.copySync(process.cwd() + `/${type}/${tplname}`, topath, {
+            errorOnExist: true
+        });
+    } catch(e) {
+        console.log("Your target folder already exists, please check!");
+        process.exit(1);
+    }
+}
+
+function modifyPkgJson(topath) {
+    try {
+        const pkgJson = fs.readJSONSync(topath + '/package.json');
+        fs.writeJsonSync(topath + '/package.json', Object.assign(pkgJson, {name: topath.split("/").reverse()[0]}), {spaces: 2})
+    } catch(e) {
+        console.log("Modify `package.json` failed! Message: " + e.message);
+        process.exit(1);
+    }
+}
+
+function main() {
+    const tplname = argv[2];
+    const type = "scaffold";
+    if(!tplname) {
+        console.log("[Error] Empty scaffold name, exit!");
+        process.exit(1);
+    }
+    if(!fs.existsSync(process.cwd() + `/${type}/${tplname}`)) {
+        console.log("[Error] No exist scaffold named '"+ tplname +"', please check!");
+        process.exit(1);
+    }
+    const topath = argv[3];
+    if(!topath) {
+        console.log("[Warning] Empty folder name, use scaffold name instead.");
+        topath = tplname;
+    }
+    execCopy(type, tplname, topath);
+    modifyPkgJson(topath);
+}
+
+main();
